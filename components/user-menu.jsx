2809189a92551984e5b14cbc1ad22c716860314e
tramtran-helen <tramtran.helen@gmail.com>
2025-07-20 20:28:40 -0400

    update user-menu

--- components/user-menu.jsx
+++ components/user-menu.jsx
@@ -1,5 +1,6 @@
-import react from 'react'
+'use client'
 import { UserButton } from '@clerk/nextjs'
+import { ChartNoAxesGantt } from 'lucide-react'
 
 
 
@@ -8,7 +9,13 @@ import { UserButton } from '@clerk/nextjs'
 const UserMenu = () => {
     return(
         <UserButton>
-
+            <UserButton.MenuItems>
+                <UserButton.Link 
+                label='Dashboard' 
+                labelIcon={<ChartNoAxesGantt size={15}/>}
+                href='/dashboard'/>
+                <UserButton.Action label='manageAccount'/>
+            </UserButton.MenuItems>
         </UserButton>
     )
 }
