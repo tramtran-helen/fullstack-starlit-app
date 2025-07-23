d27995ef9f1144733e401b935ba5c240c70837e7
tramtran-helen <tramtran.helen@gmail.com>
2025-07-23 14:57:40 -0400

    update create new collection

--- app/(main)/dashboard/_components/collection-preview.jsx
+++ app/(main)/dashboard/_components/collection-preview.jsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Link, Plus } from "lucide-react"
+
 const colorSchemes = {
     unorganized: {
         bg: 'bg-purple-950',
@@ -29,13 +31,22 @@ const CollectionPreview = ({
     onCreateNew
 }) => {
     if(isCreateNew) {
-        return <button className='relative group h-[200px] cursor-pointer'>
+        return <button onClick={onCreateNew} className='relative group h-[200px] cursor-pointer'>
             <FolderTab colorClass={colorSchemes['createCollection'].bg}/>
+            <div className={`relative h-full rounded-lg p-6 shadow-md hover:shadow-lg transition-all
+                        flex flex-col items-center justify-center gap-4 bg-white text-purple-700 border border-purple-200`}>
+                <div className='h-12 w-12 rounded-full bg-purple-100 group-hover:bg-purple-200 flex items-center justify-center'>
+                    <Plus className='h-6 w-6 text-purple-600'/>
+                </div>
+                <p>Create New Collection!</p>
+            </div>
         </button>
     }
 
     return(
-        <div>CollectionPreview</div>
+        <Link href={`/collection/${isUnorganized ? 'unorganized' : id}`} className='group relative'>
+            <FolderTab colorClass={colorSchemes[isUnorganized ? 'unorganized' : 'collection'].tab} />
+        </Link>
     )
 }
 
