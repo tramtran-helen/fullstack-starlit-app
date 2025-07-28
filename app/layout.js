bc72cda46b7f34e5edea44d5b201d0831bc13725
tramtran-helen <tramtran.helen@gmail.com>
2025-07-28 09:57:26 -0700

    client-server conflict

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-'use client'
-
 import "./globals.css";
 import { Poppins } from 'next/font/google'
 import Header from '../components/header.jsx'
--- components/header.jsx
+++ components/header.jsx
@@ -1,4 +1,5 @@
-import react from 'react'
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
